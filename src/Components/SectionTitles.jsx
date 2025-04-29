3446dd9bd03e3d0b7555a37465ffc19bbe6fc028
Ashraf Manna <optimisticashraf@gmail.com>
2025-04-29 13:53:45 +0600

    section header updated for dark mode

--- src/Components/SectionTitles.jsx
+++ src/Components/SectionTitles.jsx
@@ -3,8 +3,8 @@
 const SectionTitles = ({heading, subHeading}) => {
     return (
         <div>
-            <h1 className="text-center text-3xl font-bold text-secondary mt-5  mb-5 lg:mt-20 dark:text-green-900">{heading}</h1>
-            <p className="text-center text-lg font-semibold text-green-500 dark:text-green-900">{subHeading}</p>
+            <h1 className="text-center text-3xl font-bold text-secondary mt-5  mb-5 lg:mt-20 dark:text-white">{heading}</h1>
+            <p className="text-center text-lg font-semibold text-green-500 dark:text-gray-200">{subHeading}</p>
         </div>
     );
 };
