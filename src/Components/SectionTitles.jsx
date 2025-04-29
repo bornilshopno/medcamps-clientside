8816aefff4a01abdd5417f02c7f4d85a0c4149e6
Ashraf Manna <optimisticashraf@gmail.com>
2025-04-29 13:40:21 +0600

    section title CSS updated

--- src/Components/SectionTitles.jsx
+++ src/Components/SectionTitles.jsx
@@ -3,8 +3,8 @@
 const SectionTitles = ({heading, subHeading}) => {
     return (
         <div>
-            <h1 className="text-center text-3xl font-bold text-secondary mt-5 mb-5 lg:mt-10 dark:text-green-900">{heading}</h1>
-            <p className="text-center text-lg font-semibold text-primary dark:text-green-900">{subHeading}</p>
+            <h1 className="text-center text-3xl font-bold text-secondary mt-5  mb-5 lg:mt-20 dark:text-green-900">{heading}</h1>
+            <p className="text-center text-lg font-semibold text-green-500 dark:text-green-900">{subHeading}</p>
         </div>
     );
 };
